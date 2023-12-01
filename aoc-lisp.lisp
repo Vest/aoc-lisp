bf8436c34378b239aab26c1f0fd1c57c412d7e96
Vest <Vest@users.noreply.github.com>
2023-12-01 13:57:02 +0100

    Can show the input file (first line) using the function.

--- aoc-lisp.lisp
+++ aoc-lisp.lisp
@@ -4,3 +4,9 @@
 
 (defun hello()
   (print "test Vest"))
+
+(defun show-input (day)
+  (let* ((fileName (format nil "input~2,'0d.txt" day))
+         (fileStream (open fileName)))
+    (format t "~a~%" (read-line fileStream))
+    (close fileStream)))
