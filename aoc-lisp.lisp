ab7d2fe3e37a03c4d7f3e948cc31efd37118e9fa
Vest <Vest@users.noreply.github.com>
2023-12-01 15:04:31 +0100

    Day 1: wrote few helper-functions.

--- aoc-lisp.lisp
+++ aoc-lisp.lisp
@@ -8,6 +8,20 @@
 (defun show-input (day)
   (let* ((fileName (format nil "input~2,'0d.txt" day))
          (fileStream (open fileName)))
-    (loop for line = (read-line fileStream nil)
+    (loop
+      for line = (read-line fileStream nil)
           while line do (format t "~a~%" line))
     (close fileStream)))
+
+(defun get-left-number (str)
+  (loop
+    for c across str thereis (digit-char-p c)))
+
+(defun get-right-number (str)
+  (loop
+    for c across (reverse str) thereis (digit-char-p c)))
+
+(defun get-number (str)
+  (let* ((left (get-left-number str))
+        (right (get-right-number str)))
+    (+ (* left 10) right)))
